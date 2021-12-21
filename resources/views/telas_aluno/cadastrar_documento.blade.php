0326aa5a9b03f7873c3e33b0a1bffc452c65a139
Gabriel Rodrigues <GABRIEL.RTLIMA@UPE.BR>
2021-12-21 12:40:00 -0300

    especificando documentos da biblioteca no cadastro

--- resources/views/telas_aluno/cadastrar_documento.blade.php
+++ resources/views/telas_aluno/cadastrar_documento.blade.php
@@ -67,12 +67,12 @@
                         <div class="row">
                             <div class="col-md-12" style="margin-left: 15px;">
                                 <div class="form-group">
-                                    <label for="autor">Autor<span style="color: red">*</span>:</label>
+                                    <label for="autor">Autor: <span style="color: red">*</span></label>
                                     <input type="text" class="form-control" id="autor"
                                            placeholder="Digite o nome do Autor" value="">
                                 </div>
                                 <div class="form-group">
-                                    <label for="titulo">Titulo<span style="color: red">*</span>:</label>
+                                    <label for="titulo">Titulo: <span style="color: red">*</span></label>
                                     <input type="text" class="form-control" id="titulo"
                                            placeholder="Digite o Titulo" value="">
                                 </div>
@@ -82,22 +82,22 @@
                                            placeholder="Digite o Subtitulo" value="">
                                 </div>
                                 <div class="form-group">
-                                    <label for="local">Local<span style="color: red">*</span>:</label>
+                                    <label for="local">Local: <span style="color: red">*</span></label>
                                     <input type="text" class="form-control" id="local"
                                            placeholder="Digite o Local" value="">
                                 </div>
                                 <div class="form-group">
-                                    <label for="ano">Ano<span style="color: red">*</span>:</label>
+                                    <label for="ano">Ano: <span style="color: red">*</span></label>
                                     <input class="form-control" type="number" min="1900" max="2099" step="1"
                                            value="{{date('Y')}}"/>
                                 </div>
                                 <div class="form-group">
-                                    <label for="folhas">Folhas<span style="color: red">*</span>:</label>
+                                    <label for="folhas">Folhas: <span style="color: red">*</span></label>
                                     <input type="number" class="form-control" id="folhas"
                                            placeholder="Digite a Quantidade de Folhas" value="">
                                 </div>
                                 <div class="form-group">
-                                    <label for="ilustracao">Ilustração<span style="color: red">*</span></label>
+                                    <label for="ilustracao">Ilustração <span style="color: red">*</span></label>
                                     <select class="form-control" id="ilustracao">
                                         <option value="colorido">Colorido</option>
                                         <option value="preto_branco">Preto e Branco</option>
@@ -119,13 +119,13 @@
                             <div class="row">
                                 <div class="col-md-12" style="margin-left: 15px;">
                                     <div class="form-group">
-                                        <label for="orientador">Orientador:</label>
+                                        <label for="orientador">Orientador: <span style="color: red">*</span></label>
                                         <input type="text" class="form-control" id="orientador"
                                                placeholder="Digite o Orientador" value="">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="subtitulo">Titulação do Orientador:</label>
+                                        <label for="subtitulo">Titulação do Orientador: <span style="color: red">*</span></label>
                                         <select class="form-control" id="titulacao_orientador">
                                             <option value="graduado">Graduado</option>
                                             <option value="especialista">Especialista</option>
@@ -135,7 +135,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="orientador">Coorientador:</label>
+                                        <label for="orientador">Coorientador: </label>
                                         <input type="text" class="form-control" id="coorientador"
                                                placeholder="Digite o Nome do Coorientador" value="">
                                     </div>
@@ -149,11 +149,159 @@
                                             <option value="doutor">Doutor</option>
                                         </select>
                                     </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Curso: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="curso"
+                                               placeholder="Digite o Nome do Curso" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Campus: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="campus"
+                                               placeholder="Digite o Nome do Campus" value="">
+                                    </div>
                                 </div>
                             </div>
                             <hr>
-                        @elseif(null)
+                        @elseif($tipo_documento == "Tese")
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Tese</h3>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12" style="margin-left: 15px;">
+                                    <div class="form-group">
+                                        <label for="orientador">Orientador: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="orientador"
+                                               placeholder="Digite o Orientador" value="">
+                                    </div>
 
+                                    <div class="form-group">
+                                        <label for="titulo_orientador">Titulação do Orientador: <span style="color: red">*</span></label>
+                                        <select class="form-control" id="titulacao_orientador">
+                                            <option value="graduado">Graduado</option>
+                                            <option value="especialista">Especialista</option>
+                                            <option value="mestre">Mestre</option>
+                                            <option value="doutor">Doutor</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="coorientador">Coorientador:</label>
+                                        <input type="text" class="form-control" id="coorientador"
+                                               placeholder="Digite o Nome do Coorientador" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="titulo-coorientador">Titulação do Coorientador:</label>
+                                        <select class="form-control" id="titulacao_coorientador">
+                                            <option value="graduado">Graduado</option>
+                                            <option value="especialista">Especialista</option>
+                                            <option value="mestre">Mestre</option>
+                                            <option value="doutor">Doutor</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="programa">Programa: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="programa"
+                                               placeholder="Digite o Nome do Programa" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Campus: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="campus"
+                                               placeholder="Digite o Nome do Campus" value="">
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                        @elseif($tipo_documento == "TCC")
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Trabalho de Conclusão de Curso</h3>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12" style="margin-left: 15px;">
+                                    <div class="form-group">
+                                        <label for="orientador">Orientador: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="orientador"
+                                               placeholder="Digite o Orientador" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="subtitulo">Titulação do Orientador: <span style="color: red">*</span></label>
+                                        <select class="form-control" id="titulacao_orientador">
+                                            <option value="graduado">Graduado</option>
+                                            <option value="especialista">Especialista</option>
+                                            <option value="mestre">Mestre</option>
+                                            <option value="doutor">Doutor</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="orientador">Coorientador: </label>
+                                        <input type="text" class="form-control" id="coorientador"
+                                               placeholder="Digite o Nome do Coorientador" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="subtitulo">Titulação do Coorientador:</label>
+                                        <select class="form-control" id="titulacao_coorientador">
+                                            <option value="graduado">Graduado</option>
+                                            <option value="especialista">Especialista</option>
+                                            <option value="mestre">Mestre</option>
+                                            <option value="doutor">Doutor</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Curso: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="curso"
+                                               placeholder="Digite o Nome do Curso" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Campus: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="campus"
+                                               placeholder="Digite o Nome do Campus" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="referencias">Referências: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="referencias"
+                                               placeholder="Digite as referências" value="">
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                        @elseif($tipo_documento == "ProdutoEduc")
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Produto Educacional</h3>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12" style="margin-left: 15px;">
+
+                                    <div class="form-group">
+                                        <label for="programa">Programa: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="programa"
+                                               placeholder="Digite o Nome do Programa" value="">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="campus">Campus: <span style="color: red">*</span></label>
+                                        <input type="text" class="form-control" id="campus"
+                                               placeholder="Digite o Nome do Campus" value="">
+                                    </div>
+
+                                </div>
+                            </div>
+                            <hr>
                         @endif
                         <div class="row">
                             <div class="col-md-12">
@@ -192,6 +340,11 @@
                                            placeholder="Quinta Palavra Chave" value="">
                                 </div>
                             </div>
+                            <div class="col text-center" style="padding-top: 0px">
+                                <a type="button" class="btn btn-secondary" style="margin-right: 10px" href="{{ route('prepara-requisicao-bibli') }}">Voltar</a>
+                                <a type="submit" class="btn btn-primary-lmts" style="margin-left: 10px" href="#">Enviar</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
