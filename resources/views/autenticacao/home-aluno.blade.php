aa3aabb2348d8bab8b93fb69f25783b083a22f86
Ana Beatriz <anabiaavanderlei@gmail.com>
2022-03-13 18:04:53 -0100

    criação das classes do css + modificações na home do aluno + modificações no preenchimento do formulário pelo aluno

--- resources/views/autenticacao/home-aluno.blade.php
+++ resources/views/autenticacao/home-aluno.blade.php
@@ -8,44 +8,49 @@
 @endsection
 
 <div class="container mt-5" style="border-bottom: var(--background) 3px solid;">
-    <span style="color: var(--textcolor); font-size: 40px; font-family: 'Segoe UI'; font-weight: 700;">Solicitações</span>
+    <span class="titulo">Solicitações</span>
 </div>
 
 <div class="container text-center">
     <div>@include('componentes.mensagens')</div>
+
     <div class="row justify-content-between mt-5">
-        <div class="text-center p-5 shadow align-middle" style="background-color: var(--escolaridade); border-radius: 1rem; width: 250px;">
+
+        <div class="text-center p-5 shadow caixaSelecao" style="background-color: var(--escolaridade);">
             <a href="{{ route("prepara-requisicao")}}">
                 <div class="text-center pt-4">
                     <img class="pb-3" src="images/escolaridade-aluno.svg" height="120px">
                 </div>
                 <div class="text-center">
-                    <div style="color: white; font-weight: 600; font-family: 'Segoe UI'; font-size: 23px">Escolaridade</div>
+                    <div class="textoCaixa">Escolaridade</div>
                 </div>
             </a>
         </div>
-        <div class="text-center p-5 shadow" style="background-color: var(--biblioteca); border-radius: 1rem; width: 250px;">
+
+        <div class="text-center p-5 shadow caixaSelecao" style="background-color: var(--biblioteca)">
             <a href="{{ route("prepara-requisicao-bibli")}}">
                 <div class="col-md-12 pt-4">
                     <div class="text-center">
                         <img class="pb-3" src="images/biblioteca-aluno.svg" height="120px">
                     </div>
                     <div class="text-center align-middle">
-                        <div style="color: white; font-weight: 600; font-family: 'Segoe UI'; font-size: 23px">Biblioteca</div>
+                        <div class="textoCaixa">Biblioteca</div>
                     </div>
                 </div>
             </a>
         </div>
-        <div class="text-center p-5 shadow" style="background-color: var(--caixa); border-radius: 1rem; width: 250px;">
+
+        <div class="text-center p-5 shadow caixaSelecao">
             <a href="{{ route("listar-requisicoes-aluno", ["titulo" => "Listar Documentos Solicitados"]) }}">
                 <div class="text-center">
                     <img class="" src="images/vertical_split.svg" height="120px">
                 </div>
                 <div class="text-center">
-                    <div style="color: var(--textcolor); font-weight: 600; font-family: 'Segoe UI'; font-size: 23px">Listagem de Documentos</div>
+                    <div style="color: var(--textcolor)" class="textoCaixa">Listagem de Documentos</div>
                 </div>
             </a>
         </div>
+
     </div>
 </div>
 @endsection
