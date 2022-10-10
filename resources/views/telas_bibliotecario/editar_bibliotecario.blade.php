fd5947dfdcef08c3deca4bfd6fffe283838fab00
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-10-10 15:54:52 -0300

    corrige página de editar bibliotecario

--- resources/views/telas_bibliotecario/editar_bibliotecario.blade.php
+++ resources/views/telas_bibliotecario/editar_bibliotecario.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('conteudo')
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-4 corpoRequisicao shadow">
@@ -13,62 +12,60 @@
 
                 <div class="col-md-12 p-0">
                     <form action="{{  route('atualizar-bibliotecario')  }}" method="POST">
-                      <div class="col-sm-12">
-                          <label for="name" class="field a-field a-field_a3 page__field ">
-                              <input id="crb" type="text" class="form-control @error('crb') is-invalid @enderror field__input a-field__input"
+                        @csrf
+                        <div class="col-sm-12 mt-3">
+                            <label for="name" class="field a-field a-field_a3 page__field ">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror field__input a-field__input"
+                                     name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
+
+                                <span class="a-field__label-wrap">
+                                    <span class="a-field__label">Nome</span>
+                                </span>
+                            </label>
+                            @error('crb')
+                                <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="col-sm-12 mt-3">
+                            <label for="crb" class="field a-field a-field_a3 page__field ">
+                                <input id="crb" type="text" class="form-control @error('crb') is-invalid @enderror field__input a-field__input"
                                      name="crb" value="{{ $bibliotecario->crb }}" required autocomplete="crb" autofocus placeholder="Digite o CRB">
 
-                              <span class="a-field__label-wrap">
-                        <span class="a-field__label">CRB</span>
-                    </span>
-                          </label>
-                          @error('crb')
-                          <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                    <strong>{{ $message }}</strong>
-                    </span>
-                          @enderror
-                      </div>
-                  </div>
-
-                <div class="row justify-content-center">
-                  <div class="col-sm-12">
-                    <label for="email" class="field a-field a-field_a3 page__field ">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
-                    name="email" value="{{ $user->email }}" required autocomplete="email" autofocus placeholder="E-Mail">
-
-                    <span class="a-field__label-wrap">
-                        <span class="a-field__label">E-mail</span>
-
-                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row justify-content-center mt-3">
-                            <div class="col-sm-12">
-                                <label for="email" class="row px-3 textoFicha">Email:
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror"
-                                           name="email" value="{{ $user->email }}" required autocomplete="email"
-                                           autofocus placeholder="E-Mail">
-                                </label>
-                                @error('email')
+                                <span class="a-field__label-wrap">
+                                    <span class="a-field__label">CRB</span>
+                                </span>
+                            </label>
+                            @error('crb')
                                 <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
-                    <strong>{{ $message }}</strong>
-                    </span>
-                                @enderror
-                            </div>
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
 
+                        <div class="col-sm-12 mt-3">
+                            <label for="email" class="field a-field a-field_a3 page__field ">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror field__input a-field__input"
+                                   name="email" value="{{ $user->email }}" required autocomplete="email" autofocus placeholder="E-Mail">
+
+                            <span class="a-field__label-wrap">
+                                <span class="a-field__label">E-mail</span>
+                            </span>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert" style="overflow: visible; display:block">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
 
                         <!-- Botões -->
                         <div class="form-group row mb-3">
-
                             <div class="col-md-6 " style="">
                                 <a style="background-color: var(--padrao); border-radius: 0.5rem; color: white; font-size: 17px"
                                    class="btn" href="{{route('perfil-bibliotecario')}}">Voltar</a>
                             </div>
-
                             <div class="col-md-6 text-right">
                                 <button
                                     style="background-color: var(--confirmar); border-radius: 0.5rem; color: white; font-size: 17px"
@@ -76,17 +73,11 @@
                                     {{ __('Salvar') }}
                                 </button>
                             </div>
-
-
                         </div>
+                    </form>
                 </div>
-                </form>
             </div>
         </div>
     </div>
-    </div>
-
-
-
 
 @endsection
