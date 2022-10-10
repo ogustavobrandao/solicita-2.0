e76800efc436e5f9d146ad6304c550062b96de77
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-10-10 13:41:43 -0300

    remove virgulas

--- resources/views/mails/alerta_deposito.blade.php
+++ resources/views/mails/alerta_deposito.blade.php
@@ -8,7 +8,7 @@
 @if($status == "Concluido")
     <div class="container" style="background-color:white">
         {{-- DEFERIMENTO --}}
-        <p><font face="Times New Roman" font size="4" color="black">Olá, {{$discente}}! </font></p>
+        <p><font face="Times New Roman" font size="4" color="black">Olá {{$discente}}! </font></p>
 
         <p><font face="Times New Roman" font size="4" color="black">O comprovante de depósito do trabalho acadêmico <strong>ESTÁ
                     DISPONÍVEL PARA RETIRADA! </strong> </font></p>
@@ -26,7 +26,7 @@
 @else
     <div class="container" style="background-color:white">
         {{-- Indeferimento --}}
-        <p><font face="Times New Roman" font size="4" color="black">Prezado, {{$discente}}! </font></p>
+        <p><font face="Times New Roman" font size="4" color="black">Prezado {{$discente}}! </font></p>
 
         <p><font face="Times New Roman" font size="4" color="black">Consta(m) pendência(s) com a biblioteca em seu nome. Por favor, entre em contato com o setor. </font></p>
 
--- resources/views/mails/alerta_nada_consta.blade.php
+++ resources/views/mails/alerta_nada_consta.blade.php
@@ -10,7 +10,7 @@
 
     <div class="container" style="background-color:white">
         {{-- DEFERIMENTO --}}
-        <p><font face="Times New Roman" font size="4" color="black">Olá, {{$discente}}! </font></p>
+        <p><font face="Times New Roman" font size="4" color="black">Olá {{$discente}}! </font></p>
 
         <p><font face="Times New Roman" font size="4" color="black">A declaração de nada consta <strong>ESTÁ
                     DISPONÍVEL PARA RETIRADA! </strong> </font></p>
@@ -28,7 +28,7 @@
 @else
     <div class="container" style="background-color:white">
         {{-- Indeferimento --}}
-        <p><font face="Times New Roman" font size="4" color="black">Prezado, {{$discente}}, </font></p>
+        <p><font face="Times New Roman" font size="4" color="black">Prezado {{$discente}}, </font></p>
 
         <p><font face="Times New Roman" font size="4" color="black">Consta(m) pendência(s) com a biblioteca em seu nome. Por favor, entre em contato com o setor. </font></p>
         </p>
