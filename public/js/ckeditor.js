38e041c14a2e6b9f7ccede735bcc178a769501ec
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-10-02 13:00:45 -0300

    checa se elemento existe antes de iniciar

--- public/js/ckeditor.js
+++ public/js/ckeditor.js
@@ -1,2 +1,4 @@
-ClassicEditor.create( document.querySelector( '.editor-ckeditor1' ));
-ClassicEditor.create( document.querySelector( '.editor-ckeditor2' ));
+$el1 = document.getElementsByClassName('editor-ckeditor1');
+$el2 = document.getElementsByClassName('editor-ckeditor2');
+if ($el1.length > 0) ClassicEditor.create( document.querySelector( '.editor-ckeditor1' ));
+if ($el2.length > 0) ClassicEditor.create( document.querySelector( '.editor-ckeditor2' ));
