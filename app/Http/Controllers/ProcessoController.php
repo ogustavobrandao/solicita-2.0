9fb037f9cdd4fd524015180ba722cd1f3eff5588
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2023-10-19 18:43:58 -0300

    Inicio da lógica de ProcessosController e criação e organização das view de formulário e de geração de pdf

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Processo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProcessoController extends Controller
 {
 
     public function tratamento(){
-        return view('processos.tratamento_excepcional');
+        $aluno = Auth::user()->aluno;
+        $user = Auth::user();
+        return view('processos.modelos_pdf.antecipacao_colacao_grau.antecipacao', compact('aluno', 'user'));
     }
     public function SolicitarTratamentoExcepcional(Request $request){
+        
         $tratamento = 'Tratamento.'. $request->doc_tratamento->extension();
         $pdf = $request->doc_tratamento->storeAs('/arquivostest', $tratamento);
         $processo = new Processo();
@@ -19,12 +23,34 @@ class ProcessoController extends Controller
         $processo->user_id = 2;
         $processo->save();
 
+        if($request->tipo_processo == 'excepcional'){
+            
+        }
+
+        if($request->tipo_processo == 'alt_cadastral'){
+            
+        }
+
+        if($request->tipo_processo == 'antecipacao'){
+            
+        }
+
+        if($request->tipo_processo == 'complementar'){
+            
+        }
+
+        if($request->tipo_processo == 'disciplina'){
+            
+        }
+
+        if($request->tipo_processo == 'educacao_fisica'){
+            
+        }
+
         return 'Deu tudo certo';
         
         
 
     }
-    public funcion tratamentoform(Request $request){
-
-    }
+  
 }
---
+++ resources/views/processos/modelos_pdf/antecipacao_colacao_grau/antecipacao.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        @page {
+            margin: 0;
+        }
+
+        body{
+            text-align: center;
+           
+            
+        }
+        .conteudo{
+
+        }
+
+    </style>
+</head>
+<body>
+    <div>
+        <p>
+            MINISTÉRIO DA EDUCAÇÃO <br>
+            UNIVERSIDADE FEDERAL DO AGRESTE DE PERNAMBUCO
+            <address>Av. Bom Pastor, s/n, Bairro Boa Vista CEP 55.292- 278 Garanhuns, PE</address>
+        </p>
+    </div>
+    <div class="conteudo">
+        <p>
+            REQUERIMENTO DE ANTECIPAÇÃO DECOLAÇÃO DE GRAU
+            À REITORIA DA UFAPE, Eu, <strong>{{$user->name}}</strong> , nº de CPF <strong>{{$aluno->cpf}}</strong>, concluinte do Curso de
+            , desta Universidade, referente ao semestre , 
+            solicito Colação de Grau Antecipada, nos termos do Artigo 148, Parágrafo2º do Regimento 
+            Geral da UFRPE, pelo motivo abaixoespecificado, conforme documentaçãocomprobatória anexada a 
+            este Requerimento, peloque assumo total responsabilidade pela suaveracidade:
+            Motivo do Requerimento: 
+            Garanhuns, 
+            Contatos:
+            Email: 
+        </p>
+    </div>
+    <div>
+        <p>Obs.: Requerimento realizado pelo(a) discente através do Solicita <a href="www.solicita.ufape.edu.br">(www.solicita.ufape.edu.br)</a>, dispensa assinatura.</p>
+    </div>
+</body>
+</html>
