e3ba99bd0be999e8607cd35a30770daf1a07f698
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2023-10-25 21:50:34 -0300

    Desenvolvimento das views do pdf.2

--- resources/views/processos/modelos_pdf/antecipacao_colacao_grau/antecipacao.blade.php
+++ resources/views/processos/modelos_pdf/antecipacao_colacao_grau/antecipacao.blade.php
@@ -6,23 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        @page {
-            margin: 0;
-        }
 
         body{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            font-size: 13pt;
+            margin-left: 2cm;
+            margin-right: 2cm;
+            
+        }
+        header{
             text-align: center;
-            font-family: Calibri, 'Trebuchet MS', sans-serif; 
-           
-            
         }
-        .conteudo{
+        main{
+            text-align: justify;
+        }
 
+        footer{
+            text-align: justify;
         }
-        p #footer{
-            
 
+        h4{
+            text-align: center;
         }
+    
         address{
             display: inline;
         }
@@ -34,37 +40,45 @@
     </style>
 </head>
 <body>
-    <div>
+    <header>
         <div>
-            <img src="{{public_path('images/Logo_gov.png')}}">
+            <img src="{{public_path('images/Logo_gov.png')}}" >
         </div>
         <p>
             MINISTÉRIO DA EDUCAÇÃO <br>
             UNIVERSIDADE FEDERAL DO AGRESTE DE PERNAMBUCO
             <address>Av. Bom Pastor, s/n, Bairro Boa Vista CEP 55.292-278 Garanhuns, PE</address>
         </p>
-    </div>
-    <div class="conteudo">
+    </header>
+    <main>
+        
+            
+        <h4>
+            REQUERIMENTO DE ANOTAÇÃO DE ATIVIDADE <br>
+            CURRICULAR COMPLEMENTAR
+        </h4>
+        
+        <p>À COORDENAÇÃO DO CURSO DE {{$perfil->curso->nome}} DA UFAPE, </p>
+        <p style="text-indent: 20px;">
+            Eu <strong>{{$user->name}}</strong>, n° de CPF <strong>{{$aluno->cpf}}</strong>, aluno(a) do curso de
+            {{$perfil->curso->nome}}, solicito de V. Sa. a análise da
+            documentação apresentada em anexo, a essa
+            coordenação relativa às atividades complementares
+            para cômputo e registro de carga horária pelo
+            DRCA/UFAPE, de acordo com a resolução
+            CONSU/UFAPE no 015/2020.
+        </p>
         <p>
-            
-            <h2>
-                REQUERIMENTO DE ANTECIPAÇÃO DE COLAÇÃO DE GRAU
-            </h2>
-            
-            À REITORIA DA UFAPE, Eu, <strong>{{$user->name}}</strong>, nº de CPF {{$aluno->cpf}}, concluinte do Curso de {{$perfil->curso->nome}}, 
-            desta Universidade, referente ao semestre {{$request->semestre_conclusao}}, 
-            solicito Colação de Grau Antecipada, nos termos do Artigo 148, Parágrafo 2º do Regimento Geral da UFRPE, 
-            pelo motivo abaixo especificado, conforme documentação comprobatória anexada a este Requerimento, pelo que assumo 
-            total responsabilidade pela sua veracidade:
-            Motivo do Requerimento: {{$request->motivo}}
             Garanhuns, {{$data}}
-            
-            Contatos:
+        </p>
+        <p>
+            Contatos: <br>
             Email: {{$user->email}}
         </p>
-    </div>
-    <footer >
-        <p id="footer">Obs.: Requerimento realizado pelo(a) discente através do Solicita <a href="www.solicita.ufape.edu.br">(www.solicita.ufape.edu.br)</a>, dispensa assinatura.</p>
+        
+    </main>
+    <footer>
+        <p style="font-weight: italic">Obs.: Requerimento realizado pelo(a) discente através do Solicita <a href="www.solicita.ufape.edu.br">(www.solicita.ufape.edu.br)</a>, dispensa assinatura.</p>
     </footer>
 </body>
 </html>
