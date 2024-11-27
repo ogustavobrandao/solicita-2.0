2d28f134b251c5e149fb78ca759a77a31cd6468a
Rener1 <rener.tome123@gmail.com>
2024-11-27 14:50:17 -0300

    Script de busca e paginação refatorado visando o reuso de código. Adicionada busca à listagem de requisições do bibliotecário.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -317,6 +317,7 @@
     @include('layouts.footer')
     <!--x rodape x-->
 
+    @stack('scripts')
 </body>
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
--- resources/views/telas_admin/listar-usuarios.blade.php
+++ resources/views/telas_admin/listar-usuarios.blade.php
@@ -42,44 +42,8 @@
     </div>
 
 </div>
-
-
-<script>
-
-    $('#table').DataTable({
-        searching: true,
-
-        "language": {
-            "lengthMenu": "Mostrar _MENU_ registros por página",
-            "info": "Exibindo página _PAGE_ de _PAGES_",
-            "infoEmpty": "Nenhum registro disponível",
-            "zeroRecords": "Nenhum registro disponível",
-            "search": "",
-            "paginate": {
-                "previous": "Anterior",
-                "next": "Próximo",
-            }
-        },
-        "dom": '<"top"f>rt<"bottom"p><"clear">',
-        "order": [],
-        "columnDefs": [{
-            "targets": [2],
-            "orderable": false
-        }]
-    });
-
-    $('.dataTables_filter').addClass('here');
-    $('.dataTables_filter').addClass('');
-    $('.here').addClass('center');
-    $('.here').removeClass('dataTables_filter');
-    $('.table-hover').removeClass('dataTable');
-    $('.here').find('input').addClass('search-input');
-    $('.here').find('input').addClass('align-middle');
-    $('.here').find('label').contents().unwrap();
-    $('.here').find('input').wrap('<div class="col-md-12 my-3 py-1" style="background-color: #C2C2C2; border-radius: 1rem;"> <div class="col-md-7 my-2"> <div class="col-md-12 p-1 img-search" style="background-color: white; border-radius: 0.5rem;"> </div> </div> </div>');
-    $('.img-search').prepend('<img src="{{asset('images/search.png')}}" width="25px">');
-
-</script>
-
-
 @endsection
+
+@push('scripts')
+    <script src="{{ asset('js/datatables.js') }}"></script>
+@endpush
