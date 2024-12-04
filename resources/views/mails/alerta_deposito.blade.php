f436eea0948bcc972afb813423913ec2270c89b3
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-12-04 15:46:41 -0300

    Correção e atualização das logos dos e-mails

--- resources/views/mails/alerta_deposito.blade.php
+++ resources/views/mails/alerta_deposito.blade.php
@@ -1,14 +1,14 @@
 <div class="row justify-content-center">
     <div class="col-sm-12" align="center">
         <a target="_blank" href="http://ufape.edu.br/">
-            <img src="{{asset('/images/logo_ufape_blue_email.png')}}" style="height: 80px">
+            <img src="{{asset('/images/logo_ufape_principal.png')}}" height="80px">
         </a>
     </div>
 </div>
 @if($status == "Concluido")
     <div class="container" style="background-color:white">
         {{-- DEFERIMENTO --}}
-        <p><font face="Times New Roman" font size="4" color="black">Olá {{$discente}}! </font></p>
+        <p><font face="Times New Roman" font size="4" color="black">Olá, {{$discente}}! </font></p>
 
         <p><font face="Times New Roman" font size="4" color="black">O comprovante de depósito do trabalho acadêmico <strong>ESTÁ
                     DISPONÍVEL PARA RETIRADA! </strong> </font></p>
